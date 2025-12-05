253e38408128580d05090a9be3925d1852ebe140
TXJ-DSK <83854350+TXJ-DSK@users.noreply.github.com>
2025-12-04 22:35:07 -0600

    Update 16. 3Sum Closest.py
    Improve from O(n^2logn) to O(n^2)

--- solutions/16. 3Sum Closest.py
+++ solutions/16. 3Sum Closest.py
@@ -6,23 +6,17 @@ class Solution:
         global_min_diff = target - sum(nums[:3])
         for i in range(len(nums)-2):
             jk_target = target - nums[i]
-            for j in range(i+1, len(nums)-1):
-                k_target = jk_target - nums[j]
-                low,high = j+1, len(nums)-1
-                k = low
-                min_diff = k_target - nums[k]
-                while low <= high:
-                    mid = low + (high - low) // 2
-                    if nums[mid] <= k_target:
-                        low = mid + 1
-                    else:
-                        high = mid - 1
-                    if abs(min_diff) > abs(k_target - nums[mid]):
-                        k = mid
-                        min_diff = k_target - nums[mid]
-                if abs(min_diff) < abs(global_min_diff):
-                    global_min_diff = min_diff
-                    if min_diff == 0:
-                        return target # find a sum that equals target, end early
+            j, k = i+1, len(nums)-1
+            jk_sum = nums[j] + nums[k]
+            while(j<k):
+                jk_sum = nums[j] + nums[k]
+                if jk_sum == jk_target:
+                    return target # find a sum that equals target, end early
+                elif jk_sum < jk_target:
+                    j += 1
+                else:
+                    k -= 1
+                if abs(jk_target - jk_sum) < abs(global_min_diff):
+                    global_min_diff = jk_target - jk_sum
         return target - global_min_diff
                     
