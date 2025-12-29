a8b6a84e79c6b913a3394676753f9cd488897133
TXJ-DSK <83854350+TXJ-DSK@users.noreply.github.com>
2025-12-29 02:04:08 -0600

    Update 21. Merge Two Sorted Lists.py
    Better space complexity

--- solutions/21. Merge Two Sorted Lists.py
+++ solutions/21. Merge Two Sorted Lists.py
@@ -5,25 +5,27 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        head = ListNode(-999)
-        tail = head
-        while list1 and list2:
-            if list2.val < list1.val:
+        dummyhead = ListNode(999)
+        tail = dummyhead
+        while list1 is not None or list2 is not None:
+            if list1 is None:
                 tail.next = list2
                 list2 = list2.next
-            else:
+                tail = tail.next
+                continue
+            if list2 is None:
                 tail.next = list1
                 list1 = list1.next
-            tail = tail.next
-        if list1:
-            tail.next = list1
-        else:
-            tail.next = list2
-        return head.next
-
-
+                tail = tail.next
+                continue
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+                tail = tail.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+                tail = tail.next
+        tail.next = None
+        return dummyhead.next
         
