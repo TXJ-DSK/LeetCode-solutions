73ca9d2be5877803d39d94c2681cba325f8ec972
TXJ-DSK <83854350+TXJ-DSK@users.noreply.github.com>
2026-01-03 00:32:36 -0600

    Create 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py

---
+++ solutions/1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+        sum_thres = k * threshold
+        window_sum = sum(arr[:k])
+        count = 0
+        if window_sum >= sum_thres:
+            count += 1
+        for i in range(k, len(arr)):
+            window_sum += arr[i]
+            window_sum -= arr[i - k]
+            if window_sum >= sum_thres:
+                count += 1
+        return count
