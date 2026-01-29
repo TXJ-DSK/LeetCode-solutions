f58ab6dec190fb5d66f43c28b694442be47caa56
TXJ-DSK <83854350+TXJ-DSK@users.noreply.github.com>
2026-01-29 01:03:18 -0600

    Create 238. Product of Array Except Self.py
    O(n) time, O(1) extra space

---
+++ solutions/238. Product of Array Except Self.py
@@ -0,0 +1,11 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [0] * len(nums)
+        left, right = 1, 1
+        for i in range(len(nums)):
+            result[i] = left
+            left *= nums[i]
+        for i in range(len(nums)-1, -1, -1):
+            result[i] *= right
+            right *= nums[i]
+        return result
