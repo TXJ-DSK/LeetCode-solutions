ee3086af39c9a741d467416356456221e4871b82
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-05-09 17:14:26 -0500

    Create 200. Number of Islands.py

---
+++ solutions/200. Number of Islands.py
@@ -0,0 +1,36 @@
+# Union find solution
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        parent = [-1] * (m * n)
+        
+        def find(parent, pos, n):
+            if parent[pos] == pos:
+                return pos
+            else:
+                return find(parent, parent[pos], n)
+        
+        def unite(parent, pos1, pos2, n):
+            rep1 = find(parent, pos1, n)
+            rep2 = find(parent, pos2, n)
+            parent[rep1] = rep2
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "0":
+                    continue
+                pos = i * n + j
+                parent[pos] = pos
+                if j > 0 and grid[i][j-1] == "1":
+                    unite(parent, pos-1, pos, n)
+                if i > 0 and grid[i-1][j] == "1":
+                    unite(parent, pos-n, pos, n)
+        print(parent)
+        uniques = set()
+        for i in range(m):
+            for j in range(n):
+                pos = i * n + j
+                uniques.add(find(parent, pos, n))
+        uniques.discard(-1)
+        return len(uniques)
+        
