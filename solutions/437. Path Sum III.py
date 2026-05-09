901e541247eb7420d632fa22eb69913337d90df0
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-05-09 16:21:18 -0500

    Update 437. Path Sum III.py
    BFS O(nlogn) - O(n^2)
    DFS O(n), may cause stack overflow

--- solutions/437. Path Sum III.py
+++ solutions/437. Path Sum III.py
@@ -1,4 +1,4 @@
-# O(nlogn) for balanced tree, O(n^2) for single sided tree
+# BFS approach, O(nlogn) for balanced tree, O(n^2) for single sided tree
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         if root is None:
@@ -22,3 +22,29 @@ class Solution:
             currLevel = nextLevel
             nextLevel = []
         return result
+
+# Recursive DFS approach, O(n) time and space, may cause stack overflow for single sided tree
+import collections
+class Solution:
+    result = 0
+    target = 0
+    pathSums = Counter([0])
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        if root is None:
+            return 0
+        self.result = 0
+        self.pathSums = Counter([0])
+        self.target = targetSum
+        self.dfs(root, 0)
+        return self.result
+    
+    def dfs(self, node, rootPathSum):
+        if node is None:
+            return
+        rootPathSum += node.val
+        self.result += self.pathSums[rootPathSum - self.target]
+        self.pathSums[rootPathSum] += 1
+
+        self.dfs(node.left, rootPathSum)
+        self.dfs(node.right, rootPathSum)
+        self.pathSums[rootPathSum] -= 1
