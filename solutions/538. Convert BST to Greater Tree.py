5b2700a2a0a3d84e8d2475cebda6d196928d3e58
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-05-09 22:06:14 -0500

    Rewrite with reversed in-order traversal
    Time complexity O(n)

--- solutions/538. Convert BST to Greater Tree.py
+++ solutions/538. Convert BST to Greater Tree.py
@@ -1,31 +1,17 @@
-# DFS
-
+# DFS with reversed in-order traversal
 class Solution:
     def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root is None:
+        if not root:
             return root
-        allVals = []
-        self.dfs(root, allVals)
-        sortedVals = sorted(allVals, reverse=True)
-        cumulative = 0
-        newVals = dict()
-        for num in sortedVals:
-            cumulative += num
-            newVals[num] = cumulative
-        self.updateDfs(root, newVals)
-        return root
-    
-    def dfs(self, node, valSet):
-        valSet.append(node.val)
-        if node.left:
-            self.dfs(node.left, valSet)
-        if node.right:
-            self.dfs(node.right, valSet)
-    
-    def updateDfs(self, node, mapping):
-        node.val = mapping[node.val]
-        if node.left:
-            self.updateDfs(node.left, mapping)
-        if node.right:
-            self.updateDfs(node.right, mapping)
+        self.total = 0
+
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.right)
+            self.total += node.val
+            node.val = self.total
+            dfs(node.left)
         
+        dfs(root)
+        return root
