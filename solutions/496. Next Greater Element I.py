e069bcef7d18bddeb5fced6f93908932f7119613
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-05-11 23:44:19 -0500

    Update 496. Next Greater Element I.py

--- solutions/496. Next Greater Element I.py
+++ solutions/496. Next Greater Element I.py
@@ -1,22 +1,11 @@
+# Monotonic stack, O(N + M) time and space
 from collections import deque
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        n = len(nums1)
-        result = [-1] * n
-        numToIdx = dict()
-        for i in range(n):
-            numToIdx[nums1[i]] = i
-        stack = deque()
-        for i in range(len(nums2)-1, -1, -1):
-            num = nums2[i]
-            while len(stack) > 0 and num > stack[-1]:
-                stack.pop()
-            if len(stack) == 0:
-                stack.append(num)
-                continue
-            nge = stack[-1]
-            if num in numToIdx:
-                result[numToIdx[num]] = nge
+        nge = {num:-1 for num in nums1}
+        stack = deque() # monotonic stack DESC
+        for num in nums2:
+            while len(stack)>0 and num>stack[-1]:
+                nge[stack.pop()] = num
             stack.append(num)
-        return result
-            
+        return [nge[num] for num in nums1]
