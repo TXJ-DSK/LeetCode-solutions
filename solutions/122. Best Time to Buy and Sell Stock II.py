0af2fc953c8b32568d50505c6313f0ad8a7087c6
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-05-11 23:22:09 -0500

    Create 122. Best Time to Buy and Sell Stock II.py

---
+++ solutions/122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,8 @@
+# O(N) traversal time, O(1) space
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        total_profit = 0
+        for i in range(len(prices)-1):
+            if prices[i+1] > prices[i]:
+                total_profit += prices[i+1] - prices[i]
+        return total_profit
