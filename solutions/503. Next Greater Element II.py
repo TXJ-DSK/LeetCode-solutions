9910178080206eddf3d90fcc1d5dfab123afb933
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-05-11 23:58:06 -0500

    Update 503. Next Greater Element II.py

--- solutions/503. Next Greater Element II.py
+++ solutions/503. Next Greater Element II.py
@@ -1,22 +1,14 @@
+# monotonic stack, two loops
 from collections import deque
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        maxEle = max(nums)
-        maxEleIdx = nums.index(maxEle)
-        arr = nums[maxEleIdx:] + nums[:maxEleIdx]
-        print(arr)
-        result = [-1] * len(arr)
-        stack = deque() # Monotonic unique DESC
-        stack.append(maxEle)
-        for i in range(len(arr)-1, -1, -1):
-            num = arr[i]
-            while len(stack) > 1 and num >= stack[-1]:
-                stack.pop()
-            if num == maxEle:
-                result[i] = -1
-                continue
-            result[i] = stack[-1]
-            stack.append(num)
-        # 1 5 2 3 4 -> 5 2 3 4 1 -> 1 5 2 3 4
-        afterMaxEleLen = len(nums) - maxEleIdx
-        return result[afterMaxEleLen:] + result[:afterMaxEleLen]
+        n = len(nums)
+        result = [-1] * n
+        stack = deque() # monotonic stack DESC storing idx
+        for i in range(2*n):
+            idx = i % n
+            num = nums[idx]
+            while len(stack) > 0 and num > nums[stack[-1]]:
+                result[stack.pop()] = num
+            stack.append(idx)
+        return result
