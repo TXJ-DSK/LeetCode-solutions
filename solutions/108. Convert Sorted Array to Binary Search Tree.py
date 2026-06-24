daa60e25330a39dbedc331fa9f9bcf3e56bc388f
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-06-24 00:02:27 -0500

    Refactor sortedArrayToBST using a recursive helper
    Fixed space usage

--- solutions/108. Convert Sorted Array to Binary Search Tree.py
+++ solutions/108. Convert Sorted Array to Binary Search Tree.py
@@ -9,7 +9,13 @@ class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         if len(nums) == 0:
             return None
-        elif len(nums) == 1:
-            return TreeNode(nums[0])
-        mid = len(nums)//2
-        return TreeNode(nums[mid], self.sortedArrayToBST(nums[:mid]), self.sortedArrayToBST(nums[mid+1:]))
+        
+        def recursive(left, right):
+            if left > right:
+                return None
+            elif left == right:
+                return TreeNode(nums[left])
+            mid = left + (right - left) // 2
+            return TreeNode(nums[mid], recursive(left, mid-1), recursive(mid+1, right))
+
+        return recursive(0, len(nums)-1)
