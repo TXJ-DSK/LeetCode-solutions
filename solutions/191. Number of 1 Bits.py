910d76274157065ff23846f4fb91121eb60fadfa
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-06-29 23:49:22 -0500

    Create 191. Number of 1 Bits.py

---
+++ solutions/191. Number of 1 Bits.py
@@ -0,0 +1,8 @@
+# O(logN) Time, O(1) Space
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        result = 0
+        while n > 0:
+            result += n % 2
+            n = n // 2
+        return result
