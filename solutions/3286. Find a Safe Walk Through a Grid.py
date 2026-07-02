bec2b17c51fb98f64677ac21e9b77b67d3e4920a
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-07-02 00:11:48 -0500

    Update 3286. Find a Safe Walk Through a Grid.py

--- solutions/3286. Find a Safe Walk Through a Grid.py
+++ solutions/3286. Find a Safe Walk Through a Grid.py
@@ -1,31 +1,25 @@
 # O(M * N) Time, O(M * N) Space
 from collections import deque
+
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
         m, n = len(grid), len(grid[0])
-        #safety = [[-1 for _ in range(n)] for _ in range(m)]
-        safety = dict()
-        d = deque()
-        d.append((0, 0, health))
-        while len(d) > 0:
-            i, j, h = d.popleft()
-            #if safety[i][j] >= h:
-            if (i, j) in safety and safety[(i, j)] >= h:
-                continue
-            if h == 0:
-                continue
-            penalty = grid[i][j]
-            safety[(i, j)] = h-penalty
-            if i > 0:
-                d.append((i-1, j, h-penalty))
-            if i < m-1:
-                d.append((i+1, j, h-penalty))
-            if j > 0:
-                d.append((i, j-1, h-penalty))
-            if j < n-1:
-                d.append((i, j+1, h-penalty))
-        if (m-1, n-1) not in safety:
-            return False
-        return safety[(m-1, n-1)] > 0
-            
+        dist = [[float('inf')] * n for _ in range(m)]
+        dist[0][0] = grid[0][0]
+
+        dq = deque([(0, 0)])
+        dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+
+        while dq:
+            x, y = dq.popleft()
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n:
+                    w = grid[nx][ny]
+                    if dist[x][y] + w < dist[nx][ny]:
+                        dist[nx][ny] = dist[x][y] + w
+                        if w == 0: dq.appendleft((nx, ny))
+                        else:      dq.append((nx, ny))
+
+        return dist[m - 1][n - 1] < health
             
