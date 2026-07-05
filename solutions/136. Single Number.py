44fe14eafd99001e08ece2e1c01e9d4c2bee7877
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-07-04 23:12:35 -0500

    Create 136. Single Number.py

---
+++ solutions/136. Single Number.py
@@ -0,0 +1,7 @@
+# O(N) Time, O(1) Space
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for n in nums:
+            result = result ^ n
+        return result
