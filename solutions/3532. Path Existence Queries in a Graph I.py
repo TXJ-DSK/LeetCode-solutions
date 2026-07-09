ac6293da168919a3466a240352a618626b9aa393
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-07-08 23:50:24 -0500

    Refactor pathExistenceQueries for efficiency

--- solutions/3532. Path Existence Queries in a Graph I.py
+++ solutions/3532. Path Existence Queries in a Graph I.py
@@ -1,27 +1,11 @@
-# O(N + logN * Q) Time, O(N) Space
+# O(N + Q) Time, O(N) Space, where N is the number of nodes, Q is the number of queries
 class Solution:
     def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
-        disjoints = []
-        for i in range(n-1):
-            if nums[i+1] - nums[i] > maxDiff:
-                disjoints.append(i+0.5)
-        if len(disjoints) == 0: # all nodes are connected
-            return [True] * len(queries)
+        parent_nodes = [0] * n
+        for i in range(1, n):
+            if nums[i] - nums[i-1] > maxDiff:
+                parent_nodes[i] = i
+            else:
+                parent_nodes[i] = parent_nodes[i-1]
         
-        result = []
-        for n1, n2 in queries:
-            nsmall, nbig = min(n1, n2), max(n1, n2)
-            found_disjoint = False
-            left, right = 0, len(disjoints)-1
-            while left <= right:
-                mid = left + (right - left) // 2
-                if nsmall < disjoints[mid] < nbig:
-                    found_disjoint = True
-                    break
-                if disjoints[mid] < nsmall:
-                    left = mid + 1
-                elif disjoints[mid] > nbig:
-                    right = mid - 1
-            result.append(not found_disjoint)
-        
-        return result
+        return [parent_nodes[n1] == parent_nodes[n2] for n1, n2 in queries]
