02aecd561a43b9049826d496862c3d84d511a12b
Wenpeng Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-08-03 01:26:46 -0500

    Create 168. Excel Sheet Column Title.py

---
+++ solutions/168. Excel Sheet Column Title.py
@@ -0,0 +1,11 @@
+# O(logN) time, O(logN) space
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        ans = []
+        while(n > 0):
+            n -= 1
+            curr = n % 26
+            n = int(n / 26)
+            ans.append(chr(curr + ord('A')))
+        
+        return ''.join(ans[::-1])
