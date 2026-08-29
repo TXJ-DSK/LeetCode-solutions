2a571077dcd8ca372219133dc0c8388f99c4a08e
Wenpeng "Jerry" Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-08-28 23:55:43 -0500

    Create 349. Intersection of Two Arrays.py

---
+++ solutions/349. Intersection of Two Arrays.py
@@ -0,0 +1,5 @@
+# O(N) time, O(N) space
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1, set2 = set(nums1), set(nums2)
+        return list(set1 & set2)
