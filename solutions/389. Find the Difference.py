02e3f511c802d2171a33f827c788171566d69445
Wenpeng "Jerry" Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-09-06 22:34:08 -0700

    Create 389. Find the Difference.py

---
+++ solutions/389. Find the Difference.py
@@ -0,0 +1,6 @@
+# O(N) time, O(1) space
+from collections import Counter
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        diff = Counter(t) - Counter(s)
+        return list(diff.elements())[0]
