9421b743e503f05465dffdf4d91e7ffa605e5571
Wenpeng "Jerry" Cai <83854350+TXJ-DSK@users.noreply.github.com>
2026-09-08 23:38:03 -0700

    Create 3871. Count Commas in Range II.py

---
+++ solutions/3871. Count Commas in Range II.py
@@ -0,0 +1,9 @@
+# O(logN) time, O(1) space
+class Solution:
+    def countCommas(self, n: int) -> int:
+        result = 0
+        threshold = 1000
+        while n >= threshold:
+            result += (n - threshold + 1)
+            threshold *= 1000
+        return result
